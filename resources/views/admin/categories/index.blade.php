25e8e1307b0c7e42ae37d9a2da92524af6543b72
zbd <bulzhamur@gmail.com>
2018-09-05 23:01:24 +0800

    Форма редактирования категорий

--- resources/views/admin/categories/index.blade.php
+++ resources/views/admin/categories/index.blade.php
@@ -11,7 +11,7 @@
 
     <hr>
     <a href="{{route('admin.category.create')}}" class="btn btn-primary pull-right">
-        <i class="fa fa-plus-square-o"></i> Создать категорию </a>
+        <i class="fa fa-plus-square-o"></i>Создать категорию</a>
     <table class="table table-striped">
         <thead>
             <th>Наименование</th>
@@ -24,8 +24,8 @@
                 <td>{{$category->title}}</td>
                 <td>{{$category->published}}</td>
                 <td>
-                    <a href="{{route('admin.category.edit', ['id'=>$category->id])}}"><i
-                                class="fa fa-edit"></i></a>
+                    <a href="{{route('admin.category.edit', $category)}}"><i
+                                class="fa fa-edit">edit</i></a>
                 </td>
             </tr>
         @empty
